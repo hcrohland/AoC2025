c47e633eb18a7bf9edea61f8fd9515b6403f364d
Christoph Rohland <hcrohland@users.noreply.github.com>
2025-12-03 09:41:56 +0100

    Update template

--- src/solutions/day_template.rs
+++ src/solutions/day_template.rs
@@ -1,19 +1,33 @@
 use crate::aoc::DaySolver;
+use anyhow::{Result, bail};
 
 pub struct Solver;
 
-impl DaySolver for Solver {}
+impl DaySolver for Solver {
+    fn solve_part1(&self, _input: &Vec<String>) -> Result<i64> {
+        bail!("😱 Part 1 not yet implemented!")
+    }
+
+    fn solve_part2(&self, _input: &Vec<String>) -> Result<i64> {
+        bail!("😱 Part 2 not yet implemented!")
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
-    #[test]
-    fn test_solve_part1() {
-        assert!(true)
+    fn test_strings() -> Vec<String> {
+        vec![""].into_iter().map(|x| x.to_string()).collect()
     }
 
     #[test]
-    fn test_solve_part2() {
-        assert!(true)
+    fn test_solve_part1() -> Result<()> {
+        Ok(())
+    }
+
+    #[test]
+    fn test_solve_part2() -> Result<()> {
+        Ok(())
     }
 }
